e214777c3c71032f4aaea0d65d95361f9a136961
Brooks <brooksyang@outlook.com>
2022-03-24 14:35:00 +0800

    opt code

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,18 +1,11 @@
 package cmd
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
-
 	"goer/bootstrap"
 	"goer/global"
 
 	"github.com/gin-gonic/gin"
+	"github.com/goer-project/goer/server"
 	"github.com/spf13/cobra"
 )
 
@@ -37,45 +30,5 @@ func runServe(cmd *cobra.Command, args []string) {
 	router := bootstrap.SetupRouter()
 
 	// http server
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", global.Config.App.Port),
-		Handler: router,
-	}
-
-	// Listen and Server
-	go func() {
-		// service connections
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
-
-	// log
-	fmt.Printf("\r\n")
-	fmt.Println("Server run at:")
-	fmt.Printf("- Local: http://localhost:%d/ \r\n", global.Config.App.Port)
-	fmt.Printf("\r\n")
-	log.Printf("Enter Control + C Shutdown Server \r\n")
-
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
-	quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt)
-	<-quit
-
-	// log
-	fmt.Printf("\r\n")
-	log.Println("Shutdown Server ...")
-
-	// Timeout of 5 seconds
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	// Shutdown
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown:", err)
-	}
-
-	// log
-	log.Println("Server exiting")
+	server.Run(router, global.Config.App.Port)
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.17
 require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/validator/v10 v10.10.1
-	github.com/goer-project/goer v0.4.2
+	github.com/goer-project/goer v0.4.3
 	github.com/goer-project/goer-core v0.1.1
 	github.com/goer-project/goer-utils v0.6.0
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
