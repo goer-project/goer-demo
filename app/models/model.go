818b52a705a9d6b1b4fe35259c6f89acfaf6291b
Brooks <brooksyang@outlook.com>
2022-03-30 13:34:00 +0800

    base model

--- app/models/model.go
+++ app/models/model.go
@@ -9,6 +9,6 @@ type BaseModel struct {
 }
 
 type TimestampsField struct {
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	CreatedAt time.Time `json:"created_at" gorm:"type:DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3)"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"type:DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)"`
 }
